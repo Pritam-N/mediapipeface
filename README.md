c8c03e75bb8889f56a309f56e1e720c47f09c79d
Pritama Kumar Nayak <Pritam-N@users.noreply.github.com>
2021-05-07 21:25:38 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # mediapipeface
-Mediapipe face implementations.
+
+This is a package using mediapipe for face detection and mesh creation. 
+
+It has two modules:
+    
+    1. facedetectmodule for face detection from video/webcam
+    2. facemeshmodule for generating module from video/webcam
+
+Works well using mp4 videos. 
+
+## installation
+
+Run the following to install:
+```python
+pip install mediapipeface
+```
+
+## usage
+
+```python
+import facedetectmodule as fdm
+fdm.main('sample.mp4')
+```
+
+## develop
+```bash
+$ pip install -e .[dev]
+```
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='mediapipeface',
+    version='0.1.2',
+    description='Mediapipe face implementations.',
+    url='',
+    author='Pritama Nayak',
+    author_email='nayak264@gmail.com',
+    py_modules=['helloworld', 'facedetectmodule','facemeshmodule'],
+    package_dir={'':'src'},
+
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+
+    ],
+    install_requires = [
+        'mediapipe >= 0.8',
+        'opencv-python',
+    ],
+    extras_require = {
+        'dev': [
+            'pytest>=3.7'
+        ]
+    }
+)
